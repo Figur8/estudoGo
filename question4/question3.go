d13b30cf2694511980f79cd709ab800a43a575c0
figur8 <ismael_profissional@hotmail.com.br>
2020-03-06 15:48:19 -0300

    question1 - question3 is okay but not perfect

---
+++ question4/question3.go
@@ -0,0 +1,11 @@
+package main
+
+//Faça um programa que receba 4 valores e retorne o menor entre eles.
+import "fmt"
+
+func main() {
+	var values [4]float32
+	for index := 0; index <= len(values)); index++ {
+		
+	}
+}
